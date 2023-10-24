d347c60c1683cc847f85d6320c196736ba2650b2
Kat-Kipling <Kat.Kipling@proton.me>
2023-10-24 22:18:38 +0100

    Started account page

--- ValView/Admin/Account.aspx.cs
+++ ValView/Admin/Account.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ValView.localhost;
 
 namespace ValView.Admin
 {
@@ -11,7 +12,18 @@ namespace ValView.Admin
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if(!IsPostBack)
+            {
+                localhost.ValoViewAPI api = new localhost.ValoViewAPI();
+                try
+                {
+                    List<String> userDetails = api.getUserDetails(Session["username"].ToString()).ToList();
+                }
+                catch(Exception ex) //This shouldn't ever meet due to page being locked behind signing in - thus session variable should always exist.
+                {
 
+                }
+            }
         }
     }
 }
