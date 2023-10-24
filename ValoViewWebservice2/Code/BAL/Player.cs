6d355318c14571385397754d6d4ed9871c0ca1ed
Kat-Kipling <Kat.Kipling@proton.me>
2023-10-24 22:24:34 +0100

    Properties for players

--- ValoViewWebservice2/Code/BAL/Player.cs
+++ ValoViewWebservice2/Code/BAL/Player.cs
@@ -7,5 +7,14 @@ namespace ValoViewWebservice.App_Code.BAL
 {
     public class Player
     {
+        public int id {  get; set; }
+        public string userName { get; set; }
+        public string team {  get; set; }
+        public string country {  get; set; }
+        public string rank { get; set; }
+        public string division { get; set; }
+        public string mainRole { get; set; }
+        public string secondaryRole { get; set; }
+        public string mainAgent { get; set; }
     }
 }
